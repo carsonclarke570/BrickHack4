134468c6130362d4fa73f7a1e7d20d3e95810a67
ekanis1039 <ekanis1039@gmail.com>
2018-01-27 13:05:01 -0500

    Created Setlist_Scraper.py

---
+++ Setlist_Scraper.py
@@ -0,0 +1,10 @@
+#
+# Copyright (C) Carson Clarke-Magrab/Eric Kanis - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Carson Clarke-Magrab <ctc7359@rit.edu> & Eric Kanis <erk2974@rit.edu>, January 2018
+#
+# Setlist_Scraper.y
+#
+# Collects setlist information from setlist.fm
+#
