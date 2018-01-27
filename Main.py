dedb6ef739e3ecc56f58ac46708593dd9af17d49
carsonclarke570 <ctc7359@rit.edu>
2018-01-27 12:50:28 -0500

    -Init Commit

---
+++ Main.py
@@ -0,0 +1,6 @@
+#
+# Copyright (C) Carson Clarke-Magrab/Eric Kanis - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Carson Clarke-Magrab <ctc7359@rit.edu> & Eric Kanis <>, January 2018
+#
